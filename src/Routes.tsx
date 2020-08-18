0ecc70222278f527c261e226a41ac14ffa09f0f2
Maick Souza <maick_a_s@msn.com>
2020-08-18 16:12:36 -0300

    feat(web): consulting

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -3,11 +3,13 @@ import { BrowserRouter, Route } from 'react-router-dom';
 
 import Index from './pages/Index';
 import About from './pages/About';
+import Consulting from './pages/Consulting';
 
 const Routes: React.FC = () => (
   <BrowserRouter>
     <Route path="/" component={Index} exact />
     <Route path="/about" component={About} />
+    <Route path="/consulting" component={Consulting} />
   </BrowserRouter>
 );
 
