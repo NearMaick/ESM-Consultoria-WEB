0ef1c1063b048e2e1da3c8c0650eede66770cb88
Maick Souza <maick_a_s@msn.com>
2020-09-12 15:03:41 -0300

    style(web): regulations button

--- src/pages/Regulations/index.tsx
+++ src/pages/Regulations/index.tsx
@@ -10,16 +10,36 @@ const Regulations: React.FC = () => (
     <BurguerMenu />
     <RegulationsContent>
       <Content>
-        <Link to="/regulations/five">05</Link>
-        <Link to="/regulations/six">06</Link>
-        <Link to="/regulations/ten">10</Link>
-        <Link to="/regulations/eleven">11</Link>
-        <Link to="/regulations/twelve">12</Link>
-        <Link to="/regulations/seventeen">17</Link>
-        <Link to="/regulations/eighteen">18</Link>
-        <Link to="/regulations/twenty-three">23</Link>
-        <Link to="/regulations/thirty-three">33</Link>
-        <Link to="/regulations/thirty-five">35</Link>
+        <Link to="/regulations/five">
+          <p>05</p>
+        </Link>
+        <Link to="/regulations/six">
+          <p>06</p>
+        </Link>
+        <Link to="/regulations/ten">
+          <p>10</p>
+        </Link>
+        <Link to="/regulations/eleven">
+          <p>11</p>
+        </Link>
+        <Link to="/regulations/twelve">
+          <p>12</p>
+        </Link>
+        <Link to="/regulations/seventeen">
+          <p>17</p>
+        </Link>
+        <Link to="/regulations/eighteen">
+          <p>18</p>
+        </Link>
+        <Link to="/regulations/twenty-three">
+          <p>23</p>
+        </Link>
+        <Link to="/regulations/thirty-three">
+          <p>33</p>
+        </Link>
+        <Link to="/regulations/thirty-five">
+          <p>35</p>
+        </Link>
       </Content>
     </RegulationsContent>
   </Container>
--- src/pages/Regulations/styles.ts
+++ src/pages/Regulations/styles.ts
@@ -23,12 +23,15 @@ export const Content = styled.div`
   display: grid;
   grid-template-columns: repeat(5, 1fr);
   grid-template-rows: repeat(2, 1fr);
-  grid-gap: 1.8rem;
+  grid-gap: 2.8rem;
 
   a {
-    background: var(--color-primary);
+    background: var(--color-input-background);
     color: var(--color-text-base);
+    border: 4px double var(--color-text-base);
     border-radius: 0.5rem;
+
+    transform: rotate(45deg);
 
     display: flex;
     align-items: center;
@@ -38,6 +41,10 @@ export const Content = styled.div`
 
     width: 60px;
     height: 60px;
+
+    p {
+      transform: rotate(-45deg);
+    }
   }
 
   @media (min-width: 900px) {
